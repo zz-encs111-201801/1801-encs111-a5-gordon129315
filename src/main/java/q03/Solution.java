1bf64e80ed3ffa26d0884cf0e90284947cf9ba67
Zhuoran Yu <gordonyuzr@gmail.com>
2018-03-16 18:30:46 -0400

    3.16
    3.16

--- src/main/java/q03/Solution.java
+++ src/main/java/q03/Solution.java
@@ -2,7 +2,6 @@ package q03;
 
 
 import java.util.HashMap;
-import java.util.LinkedList;
 
 public class Solution {
 
@@ -20,25 +19,16 @@ public class Solution {
             return map.get(restOfLength);
         }
 
-        LinkedList<Integer> list = new LinkedList<>();
+        int maxPrice = 0;
         for (int i = 0; i < prices.length; i++) {
             if (prices[i][0] <= restOfLength) {
-                list.add(prices[i][1] + maxValueRecurse(prices, restOfLength - prices[i][0], map));
+                maxPrice = Math.max(maxPrice, prices[i][1] + maxValueRecurse(prices, restOfLength - prices[i][0], map));
             }
         }
 
-        int maxPrice = maxValueInList(list);
         map.put(restOfLength, maxPrice);
 
         return maxPrice;
-    }
-
-    private int maxValueInList(LinkedList<Integer> list) {
-        int max = Integer.MIN_VALUE;
-        for (int value : list) {
-            max = Math.max(max, value);
-        }
-        return max;
     }
 
 }
