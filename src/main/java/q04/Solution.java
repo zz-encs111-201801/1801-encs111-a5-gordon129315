1c9943c62792d00faa74b50ed98d8c09354d46a4
Zhuoran Yu <gordonyuzr@gmail.com>
2018-03-19 02:30:06 -0400

    3.19
    3.19

--- src/main/java/q04/Solution.java
+++ src/main/java/q04/Solution.java
@@ -1,10 +1,27 @@
 package q04;
 
 
+import java.util.Arrays;
+
 public class Solution {
 
     public int solve(int[] coins, int value) {
-        return 0;
+        Arrays.sort(coins);
+        return recursion(coins, coins.length - 1, value);
+    }
+
+    private int recursion(int[] coins, int i, int restValue) {
+        if (restValue == 0) {
+            return 1;
+        }
+
+        if (restValue < 0 || i < 0) {
+            return 0;
+        }
+
+        //选了 + 没选，从下一个开始选
+        return recursion(coins, i, restValue - coins[i]) + recursion(coins, i - 1, restValue);
+
     }
 
 }
